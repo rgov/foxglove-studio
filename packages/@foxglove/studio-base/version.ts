1eb737ed70c173e76ba8a198c3eb23aa96fe4e7f
Roman Shtylman <roman@foxglove.dev>
2021-05-17 11:08:00 -0700

    Move app to packages/@foxglove/studio-base
    Since app components are re-used across desktop and are used as a 'package'
    by these environments, we move the app folder into our packages folder to
    mirror where we store other packages in the workspace.

--- packages/@foxglove/studio-base/version.ts
+++ packages/@foxglove/studio-base/version.ts
@@ -2,7 +2,7 @@
 // License, v2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import packageJson from "../package.json";
+import packageJson from "../../../package.json";
 
 export const APP_NAME: string = packageJson.productName;
 export const APP_VERSION: string = packageJson.version;
